46b6c717bc70d2ca50ce96eaca3eab011b111dc2
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2021-03-01 01:52:59 -0600

    Changing CORS to work with frontend

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(morgan("tiny"));
 //     credentials: true,
 //   })
 // );
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
