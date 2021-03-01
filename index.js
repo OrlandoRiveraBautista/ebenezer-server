1c006ddffe96d2e03bd1f0fe15447ee96880a40e
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2021-03-01 01:04:29 -0600

    Changing CORS to work with frontend

--- index.js
+++ index.js
@@ -37,16 +37,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Middleware
 app.use(cookieParser());
 app.use(morgan("tiny"));
-const whitelist = [`${process.env.FRONT_URL}`, "http://localhost:3000"];
+// const whitelist = [`${process.env.FRONT_URL}`, "http://localhost:3000"];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    // origin:
+    // function (origin, callback) {
+    //   if (whitelist.indexOf(origin) !== -1) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error("Not allowed by CORS"));
+    //   }
+    // },
     credentials: true,
   })
 );
