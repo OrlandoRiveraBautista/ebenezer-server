ae44a91c999a6462077b923b1048a9d3edd6509c
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2021-03-01 01:45:42 -0600

    Changing CORS to work with frontend

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://ebenezer-virtual.herokuapp.com/"
+    "https://ebenezer-virtual.herokuapp.com"
   );
   res.header("Access-Control-Allow-Headers", true);
   res.header("Access-Control-Allow-Credentials", true);
